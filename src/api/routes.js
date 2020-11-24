cdfdb04c58c29d9d9bd10f97b5c9fb14c3065b37
Ricardo Jose Valladares <rick_valladares@hotmail.com>
2020-11-24 12:14:41 -0600

    pasando a heroku

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,4 +1,5 @@
-const APP_URL = "http://tgu.brothers.com";
+// const APP_URL = "http://tgu.brothers.com";
+const APP_URL = "https://blooming-brushlands-91833.herokuapp.com";
 
 export const SAVE_REGISTRO = `${APP_URL}/api/registrar`;
 export const GET_CURSOS_RESUME = `${APP_URL}/api/cursos_resume`;
