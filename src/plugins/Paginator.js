69f5f2d3bc3a7b410189a10b0f7217d2febe3144
Ricardo Jose Valladares <rick_valladares@hotmail.com>
2020-11-24 12:16:23 -0600

    arreglando warnings

--- src/plugins/Paginator.js
+++ src/plugins/Paginator.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 export const Paginator = ({pagesCount, pageActive, leftArrowAction, rightArrowAction, numbersAction}) =>{
 
