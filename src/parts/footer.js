57c28a7b84f3ff0862f85a6300fa47966ddae990
Ricardo Jose Valladares <rick_valladares@hotmail.com>
2020-11-26 10:04:15 -0600

    cambios responsive para el nav

--- src/parts/footer.js
+++ src/parts/footer.js
@@ -6,8 +6,8 @@ export const Footer = ()=>{
         <div className="footer">
             <img className="footer-logo mb-4" src={jabali} alt=""/>
             <h4>Brothers TGU</h4>
-            <p className="mb-0"><b>Tegucigalpa, Honduras</b></p>
-            <p className="mb-0"><b>Email: tegucigalpa@brotherland.com</b></p>
+            <p className="mb-0 text-center"><b>Tegucigalpa, Honduras</b></p>
+            <p className="mb-0 text-center"><b>Email: tegucigalpa@brotherland.com</b></p>
             <p className="mb-0 footer-copyright"><b>2020 | Todos los derechos reservados</b></p>
         </div>
     );
