b8d59350de20de327603a92680d7579f9a4cd411
Ricardo Jose Valladares <rick_valladares@hotmail.com>
2020-12-04 10:44:32 -0600

    ajustando nueva ruta

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,5 +1,5 @@
 // const APP_URL = "http://tgu.brothers.com";
-const APP_URL = "https://blooming-brushlands-91833.herokuapp.com";
+const APP_URL = "https://brothertgu.herokuapp.com/";
 
 export const SAVE_REGISTRO = `${APP_URL}/api/registrar`;
 export const GET_CURSOS_RESUME = `${APP_URL}/api/cursos_resume`;
